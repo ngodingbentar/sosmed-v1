da2afaadd7e17c57a3ee391f00ef4d4e952557b0
ngodiingbentar <dewaqintoro@gmail.com>
2022-01-18 01:59:07 +0700

    added: user image + create pin logo on navbar

--- fe/src/container/Pins.jsx
+++ fe/src/container/Pins.jsx
@@ -7,10 +7,11 @@ const Pins = ({user}) => {
   const [searchTerm, setSearchTerm] = useState('')
   return (
     <div className='px-2 md:px-5'>
-      <div className="bg-gray-500">
+      <div className="bg-gray-50">
         <Navbar
           searchTerm={searchTerm}
           setSearchTerm={setSearchTerm}
+          user={user}
         />
       </div>
       <div className="h-full">
