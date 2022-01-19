d2e53be219061cbd8ef60d090bb76a34912628ed
ngodiingbentar <dewaqintoro@gmail.com>
2022-01-19 23:42:29 +0700

    added destination label & delete button

--- fe/src/components/Pin.jsx
+++ fe/src/components/Pin.jsx
@@ -37,6 +37,14 @@ const Pin = ({ pin: {postedBy, image, _id, destination, save }}) => {
         })
     }
   }
+
+  const deletePin = (id) => {
+    client
+      .delete(id)
+      .then(() => {
+        window.location.reload()
+      })
+  }
   return (
     <div className='m-2'>
       <div
@@ -49,36 +57,62 @@ const Pin = ({ pin: {postedBy, image, _id, destination, save }}) => {
         {postHovered && (
           <div
             className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
-            style={{height: '100%'}}>
-              <div className="flex items-center justify-between">
-                <div className="flex gap-2">
-                  <a
-                    href={`${image?.asset?.url}?dl=`}
-                    download
-                    onClick={(e) => e.stopPropagation()}
-                    className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none '
-                  >
-                    <MdDownloadForOffline />
-                  </a>
-                </div>
-                {alreadySaved ? (
-                  <button type='button' className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none' >
-                    {save?.length} Saved
-                  </button>
-                ):(
-                  <button
-                    type='button'
-                    className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none '
-                    onClick={(e) => {
-                      e.stopPropagation()
-                      savePin(_id)
-                    }}
-                  >
-                    Save
-                  </button>
-                )}
+            style={{height: '100%'}}
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none '
+                >
+                  <MdDownloadForOffline />
+                </a>
               </div>
+              {alreadySaved ? (
+                <button type='button' className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none' >
+                  {save?.length} Saved
+                </button>
+              ):(
+                <button
+                  type='button'
+                  className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none '
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    savePin(_id)
+                  }}
+                >
+                  Save
+                </button>
+              )}
             </div>
+            <div className="flex justify-between items-center gap-2 w-full ">
+              {destination && (
+                <a
+                  href={destination}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md "
+                >
+                  <BsFillArrowUpRightCircleFill />
+                  { destination.length > 20 ? destination.slice(8, 18)+'...' : destination }
+                </a>
+              )}
+              { postedBy?._id === user.googleId && (
+                <button
+                  type='button'
+                  className='bg-white p-2 opacity-70 hover:opacity-100 font-bold text-dark text-base rounded-3xl hover:shadow-md outline-none '
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    deletePin(_id)
+                  }}
+                >
+                  <AiTwotoneDelete />
+                </button>
+              ) }
+            </div>
+          </div>
         )}
       </div>
     </div>
