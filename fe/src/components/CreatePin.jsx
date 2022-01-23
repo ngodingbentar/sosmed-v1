0f09466fa2bac4db1cf5e4fcfadc587121f885bd
ngodiingbentar <dewaqintoro@gmail.com>
2022-01-24 02:53:10 +0700

    upload section

--- fe/src/components/CreatePin.jsx
+++ fe/src/components/CreatePin.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineCloudUpload } from 'react-icons/ai'
+import { MdDelete } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
+
+import { client } from '../client'
+import Spinner from './Spinner'
+import { categories } from '../utils/data'
 
 const CreatePin = () => {
+  const [title, setTitle] = useState('')
+  const [about, setAbout] = useState('')
+  const [destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(false);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(null);
   return (
-    <div>
-      CreatePin
+    <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5 '>
+      {fields && (
+        <p className="text-red-500 mb-5 text-lg transition-all duration-150 ease-in">
+          Please fill in all the fields.
+        </p>
+      )}
+      <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full">
+        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+          <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420 ">
+            {loading && <Spinner />}
+            {wrongImageType && <p>Wrong image type</p>}
+            {!imageAsset ? (
+              <label>
+                <div className="flex flex-col items-center justify-center h-full">
+                  <div className="flex flex-col justify-center items-center">
+                    <p className="text-bold text-2xl">
+                      <AiOutlineCloudUpload />
+                    </p>
+                    <p className="text-lg">Click to upload</p>
+                  </div>
+                  <p className="mt-32 text-gray-400">
+                    Use high-quality JPG, JPEG, SVG, PNG, GIF less than 20Mb
+                  </p>
+                </div>
+                <input
+                  type="file"
+                  name="upload-image"
+                  className="w-0 h-2"
+                />
+              </label>
+            ) : (
+              <p>wadudu</p>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
