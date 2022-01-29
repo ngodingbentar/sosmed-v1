255c66f4f1dbd07d255e3fa3cb08b2683e1c7b96
ngodiingbentar <dewaqintoro@gmail.com>
2022-01-30 00:19:46 +0700

    added: render comment

--- fe/src/components/PinDetail.jsx
+++ fe/src/components/PinDetail.jsx
@@ -79,6 +79,22 @@ const PinDetail = ({user}) => {
             {pinDetail?.postedBy?.userName}
           </p>
         </Link>
+        <h2 className="mt-5 text-2xl">Comments</h2>
+        <div className="max-h-370 overflow-y-auto">
+          {pinDetail?.comments?.map((comment, i) => (
+            <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={i}>
+              <img
+                src={comment.postedBy?.image}
+                alt='user-profile'
+                className='w-10 h-10 rounded-full cursor-pointer'
+              />
+              <div className="flex flex-col">
+                <p className="font-bold">{ comment.postedBy.userName }</p>
+                <p>{ comment.comment }</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
